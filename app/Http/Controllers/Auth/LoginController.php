d0420b6976cdb48d95715d6d95ddec02043e9d50
chamod wijesena <chamodwijesena77@gmail.com>
2021-08-30 16:41:27 +0530

    user registration and login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/customer_save';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/customer_save', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
